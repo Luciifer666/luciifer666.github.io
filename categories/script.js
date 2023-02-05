2127311920525f8cc80445c78287e023a63c5be2
Lucifer666 <52002346+Luciifer666@users.noreply.github.com>
2023-02-05 12:50:22 +0100

    Update script.js

--- categories/script.js
+++ categories/script.js
@@ -4,10 +4,10 @@ var confirmation;
 links.forEach(link => {
   link.addEventListener("click", function(event) {
 
-    if(this.href === "http://127.0.0.1:3000/index.html") {
+    if(this.href === "https://luciifer666.github.io/index.html") {
       confirmation = confirm(`Voulez-vous vous rediriger au menu Home ?`);
     } else {
-      confirmation = confirm(`Voulez-vous vraiment vous rediriger vers ${this.text} ? (${this.href.replace("http://127.0.0.1:3000/pages/", "")})`);
+      confirmation = confirm(`Voulez-vous vraiment vous rediriger vers ${this.text} ? (${this.href.replace("https://luciifer666.github.io/pages/", "")})`);
     }
     if (!confirmation) {
       event.preventDefault();
