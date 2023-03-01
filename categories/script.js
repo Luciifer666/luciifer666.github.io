014d9e8568aa0cf88cde418cbb0b230abd1c5763
Lucifer666 <52002346+Luciifer666@users.noreply.github.com>
2023-03-01 17:09:41 +0100

    changing language

--- categories/script.js
+++ categories/script.js
@@ -5,11 +5,11 @@ links.forEach(link => {
   link.addEventListener("click", function(event) {
 
     if(this.href === "https://luciifer666.github.io/index.html") {
-      confirmation = confirm(`Voulez-vous vous rediriger au menu Home ?`);
+      confirmation = confirm(`Do you want to be redirected to the Home menu ?`);
     } else if(this.href === "https://luciifer666.github.io/categories/first/cubes.html") {
-      confirmation = confirm("Voulez-vous aller dans la section des cubes lumineux ?");
+      confirmation = confirm("Do you want to go to the light cubes section ?");
     } else {
-      confirmation = confirm(`Voulez-vous vraiment vous rediriger vers ${this.text} ? (${this.href.replace("https://luciifer666.github.io/pages/", "")})`);
+      confirmation = confirm(`Do you really want to be redirected to ${this.text} ? (${this.href.replace("https://luciifer666.github.io/pages/", "")})`);
     }
     if (!confirmation) {
       event.preventDefault();
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var links = document.querySelectorAll("li a");
 
 for(var i = 0; i < links.length; i++) {
     links[i].addEventListener("click", function(event) {
-        const confirmation = confirm(`Voulez-vous être redirigé vers ${this.text} ?`);
+        const confirmation = confirm(`Do you want to be redirected to ${this.text} ?`);
 
         if(!confirmation) event.preventDefault();
     });
