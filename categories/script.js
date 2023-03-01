340874fe598c4fc21c9abf30b13c8ec1bf05e145
Lucifer666 <52002346+Luciifer666@users.noreply.github.com>
2023-03-01 16:50:08 +0100

    adding cube animation effects

--- categories/script.js
+++ categories/script.js
@@ -6,6 +6,8 @@ links.forEach(link => {
 
     if(this.href === "https://luciifer666.github.io/index.html") {
       confirmation = confirm(`Voulez-vous vous rediriger au menu Home ?`);
+    } else if(this.href === "https://luciifer666.github.io/categories/first/cubes.html") {
+      confirmation = confirm("Voulez-vous aller dans la section des cubes lumineux ?");
     } else {
       confirmation = confirm(`Voulez-vous vraiment vous rediriger vers ${this.text} ? (${this.href.replace("https://luciifer666.github.io/pages/", "")})`);
     }
