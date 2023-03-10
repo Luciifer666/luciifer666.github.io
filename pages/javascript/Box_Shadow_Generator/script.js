15e80aa7af09f02d7d4e44e4069ab0c58a034791
Lucifer666 <52002346+Luciifer666@users.noreply.github.com>
2023-03-11 00:56:59 +0100

    adding box shadow generator

---
+++ pages/javascript/Box_Shadow_Generator/script.js
@@ -0,0 +1,56 @@
+let elem = document.getElementById("element");
+let code = document.getElementById("code");
+let inputs = document.querySelectorAll(".sliders input");
+
+inputs.forEach((inp) => {
+    inp.addEventListener("input", generateShadow);
+});
+
+function generateShadow() {
+    let hShadow = document.getElementById("h-shadow").value;
+    let vShadow = document.getElementById("v-shadow").value;
+    let blurRadius = document.getElementById("blur-radius").value;
+    let spreadRadius = document.getElementById("spread-radius").value;
+    let shadowColor = document.getElementById("shadow-color").value;
+    let shadowColorOpacity = document.getElementById(
+      "shadow-color-opacity"
+    ).value;
+    let shadowInset = document.getElementById("shadow-inset").checked;
+
+    //using ternary operator to check if inset checkbox is checked or not
+    //if checked we add the inset prefix, else no inset prefix is added
+
+    let boxShadow = shadowInset
+    ? `inset ${hShadow}px ${vShadow}px ${blurRadius}px ${spreadRadius}px ${hexToRgba(
+        shadowColor,
+        shadowColorOpacity
+      )}`
+    : `${hShadow}px ${vShadow}px ${blurRadius}px ${spreadRadius}px ${hexToRgba(
+        shadowColor,
+        shadowColorOpacity
+      )}`;
+
+    elem.style.boxShadow = boxShadow;
+    code.textContent = `box-shadow: ${boxShadow};`;
+};
+
+//converting hex value to rgba
+
+function hexToRgba(shadowColor, shadowColorOpacity) {
+    let r = parseInt(shadowColor.substr(1, 2), 16);
+    let g = parseInt(shadowColor.substr(3, 2), 16);
+    let b = parseInt(shadowColor.substr(5, 2), 16);
+    
+    return `rgba(${r},${g},${b},${shadowColorOpacity})`;
+};
+
+//copy the generated code to clipboard
+
+function copyCode() {
+    code.select();
+    document.execCommand("copy");
+    alert("Code copied to Clipboard");
+};
+
+//call the generateShadow function on every page load
+window.onload = generateShadow();
