0ebcc240b17b09e77d561dfdbd09eab77d05ab15
Lucifer666 <52002346+Luciifer666@users.noreply.github.com>
2023-03-11 23:21:48 +0100

    Update script.js

--- categories/script.js
+++ categories/script.js
@@ -8,6 +8,8 @@ links.forEach(link => {
       confirmation = confirm(`Do you want to be redirected to the Home menu ?`);
     } else if(this.href === "https://luciifer666.github.io/categories/first/cubes.html") {
       confirmation = confirm("Do you want to go to the light cubes section ?");
+    } else if(this.href === "https://luciifer666.github.io/categories/second/login.html") {
+      confirmation = confirm("Do you want to go to the page of animated logins ?")
     } else {
       confirmation = confirm(`Do you really want to be redirected to ${this.text} ? (${this.href.replace("https://luciifer666.github.io/pages/", "")})`);
     }
