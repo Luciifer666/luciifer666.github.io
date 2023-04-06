43daf8cb9115400ebbd6a1d86f3c6678c4dc8b31
Lucifer666 <52002346+Luciifer666@users.noreply.github.com>
2023-04-06 07:12:18 +0200

    adding 3D Cards

---
+++ pages/design/3D_Card_-_Ant-Man/script.js
@@ -0,0 +1,5 @@
+const reference = document.getElementById("reference");
+
+reference.addEventListener("click", () => {
+    alert("You will be redirected on the Wikipedia Page of Ant-Man and the Wasp : Quantumania")
+});
--- pages/design/3D_Movie_Card/script.js
+++
@@ -1,5 +0,0 @@
-const reference = document.getElementById("reference");
-
-reference.addEventListener("click", () => {
-    alert("You will be redirected on the Wikipedia Page of Ant-Man and the Wasp : Quantumania")
-});
