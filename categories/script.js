35f0cde5b03c89247f1ed413a01655d47353be1b
Lucifer666 <52002346+Luciifer666@users.noreply.github.com>
2023-05-31 12:34:35 +0200

    Update script.js

--- categories/script.js
+++ categories/script.js
@@ -1,5 +1,12 @@
 const links = document.querySelectorAll("a");
+const toggle = document.getElementById('theme-toggle');
 var confirmation;
+// let background = localStorage.getItem('background_page');
+
+// // values
+// const title = document.querySelector('h1');
+// const paragraph = document.querySelector('p');
+// const buttons = document.querySelector('a');
 
 links.forEach(link => {
   link.addEventListener("click", function(event) {
@@ -7,14 +14,17 @@ links.forEach(link => {
     if(this.href === "https://luciifer666.github.io/index.html") {
       confirmation = confirm(`Do you want to be redirected to the Home menu ?`);
     } else if(this.href === "https://luciifer666.github.io/categories/first/cubes.html") {
-      confirmation = confirm("Do you want to go to the light cubes section ?");
+      confirmation = confirm("Do you want to go on the light cubes page ?");
     } else if(this.href === "https://luciifer666.github.io/categories/second/login.html") {
-      confirmation = confirm("Do you want to go to the page of animated logins ?");
+      confirmation = confirm("Do you want to go on the Animated Login page ?");
     } else if(this.href === "https://luciifer666.github.io/categories/second/glassmorphism.html") {
-      alert("Glassmorphism is Glassmorphism is the latest user interface design principle (glass effects into the look)")
-      confirmation = confirm("Do you want to go to the page of the Glassmorphism ?");
+      alert("Glassmorphism is the latest user interface design principle (glass effects into the look)")
+      confirmation = confirm("Do you want to go on the Glassmorphism page ?");
+    } else if(this.href === "https://luciifer666.github.io/categories/second/parallax.html") {
+      alert("Parallax refers to the apparent shift or difference in the position of an object when viewed from different perspectives")
+      confirmation = confirm("Do you want to go on the Parallax page ?")
     } else if(this.href === "https://luciifer666.github.io/categories/fourth/user_interface.html") {
-      confirmation = confirm("Do you want to go to the page of the UI ?");
+      confirmation = confirm("Do you want to go on the UI page ?");
     } else if(this.href.startsWith("https://luciifer666.github.io/pages/events")) {
 
     // if(detectMobile()) {
@@ -29,6 +39,64 @@ links.forEach(link => {
     }
   });
 });
+
+/* 
+//Background
+
+if(background) {
+  background = background;
+} else {
+  localStorage.setItem('background_page', getComputedStyle(document.body).backgroundColor);
+  background = getComputedStyle(document.body).backgroundColor;
+};
+
+if(background === "rgb(33, 33, 33)") {
+  document.body.style.backgroundColor = background;
+} else {
+  document.body.style.backgroundColor = 'rgb(246, 246, 246)';
+  localStorage.setItem('background_page', "rgb(246, 246, 246)");
+};
+
+//default values
+
+if(background === "rgb(33, 33, 33)") {
+  title.style.color = "#bbbbbb";
+} else {
+  title.style.color = "#444444";
+};
+*/
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   if(background) {
+//     localStorage.removeItem('background_page');
+//     console.log('Page chargée ! Background supprimé');
+//     localStorage.setItem('background_page', 'rgb(246, 246, 246)');
+//   } else {
+//     console.log('Page chargée ! Aucun précédent background sauvegardé.');
+//     localStorage.setItem('background_page', 'rgb(246, 246, 246)');
+//   };
+// });
+
+// toggle.addEventListener('click', function() {
+//   var body = document.body;
+
+//   if(background === 'rgb(246, 246, 246)') {
+//     background = 'rgb(33, 33, 33)';
+//     body.style.backgroundColor = 'rgb(33, 33, 33)';
+//     localStorage.setItem('background_page', 'rgb(33, 33, 33)');
+
+//     title.style.color = "#bbbbbb";
+//     paragraph.style.color = '#cccccc';
+//     buttons.style.backgroundColor = '#123456'
+//   } else {
+//     background = 'rgb(246, 246, 246)';
+//     body.style.backgroundColor = 'rgb(246, 246, 246)';
+//     localStorage.setItem('background_page', 'rgb(246, 246, 246)');
+
+//     title.style.color = "#444444";
+//     paragraph.style.color = '#333'; // #333333
+//   }
+// });
 
 // function detectMobile() {
 //   const toMatch = [
